787302db01f2ab46eeb1e394c612a706b8751b50
Erica Thompson <younique98@gmail.com>
2021-02-19 07:26:18 -0800

    fixed  Cannot read property 'then' of undefined     at ProductList issue by placing a return within the getProducts function

--- scripts/products/ProductList.js
+++ scripts/products/ProductList.js
@@ -7,11 +7,14 @@ const contentTarget = document.querySelector(".menu__items")
 
 let bakeryProducts = []
 let bakeryCategories = []
+let category = []
 
 export const ProductList = () => {
+  debugger
   getProducts()
-    .then(getCategories())
-    .then(() => {
+    
+  .then(() => {
+      category = getCategories()
       bakeryProducts = useProducts()
       bakeryCategories = useCategories()
       render()
@@ -20,10 +23,9 @@ export const ProductList = () => {
 
 const render = () => {
   contentTarget.innerHTML = bakeryProducts.map(product => {
-    const productCategory = bakeryCategories.find(category => cat.id === product.categoryId)
+    const productCategory = bakeryCategories.find(category => category.id === product.categoryId)
 
     return Product(product, productCategory)
   }).join("")
 }
 
-// eventHub.addEventListener()
